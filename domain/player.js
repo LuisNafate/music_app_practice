c017dfb369b0edd3cdc9c11d31abbd77b1a003ae
Luis Nafate <luisnafate51@gmail.com>
2025-09-30 11:07:47 -0600

    Avance de refactorizacion de main.js

--- domain/player.js
+++ domain/player.js
@@ -1,5 +1,10 @@
 export default {
     _nextSongs: [],
+    _lastSongs: [],
+    _actualSong: null,
+    _progress_bar: document.getElementById("progress"),
+    _media: document.getElementById("media"),
     
+
 
 }
